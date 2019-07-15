7e3b9e00aa8936643b352e76e2574b47843e36ac
ivanavatkov <vanes700i@yandex.ru>
2019-07-15 22:02:35 +0300

    Initial commit

---
+++ Lesson 01/ClassWork/Lesson01_ConsoleApp/Lesson01_ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace Lesson01_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.ASCII;
+            Console.OutputEncoding = Encoding.ASCII;
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(3000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
