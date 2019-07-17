09fb062ba29365a274493cfcd650cc0e6849ce3e
ivanavatkov <vanes700i@yandex.ru>
2019-07-17 22:15:47 +0300

    Commit lesson 02 end

--- Lesson 02/ClassWork/Lesson 02/Lesson 02/Program.cs
+++ Lesson 02/ClassWork/Lesson 02/Lesson 02/Program.cs
@@ -17,6 +17,28 @@ namespace Lesson_02
 
             Console.WriteLine($"{ a} * { b} = { a* b}");
 
+            byte maximum = 255;
+            sbyte maximumSignedByte = sbyte.MaxValue;
+            Console.WriteLine(maximum);
+            int sun = 1_345_432;
+            long universe = sun;
+            sun = (int)universe;
+            float f = sun;
+            sun = (int)f;
+            /* 
+            Так как это оператор приведения, нужно писать желаемый тип переменной. 
+            Так как Sun уже заявлена как int, пишем int перед названием переменной.
+            */
+            string valueOfSun = sun.ToString();
+            Console.WriteLine(valueOfSun);
+            Console.WriteLine(sun);
+            Console.Write("Enter nuberic value: "); 
+            string value = Console.ReadLine();
+            int integerValue = int.Parse(value); //перевожу текстовую информацию в целочисленную (парсинг). 
+            Console.WriteLine (value + value);
+            Console.WriteLine (integerValue + integerValue);
+            var s = 175;
+            bool ss = 175 == 5;
         }
     }
 }
---
+++ Lesson 02/HomeWork/HomeWork Lesson 2/HomeWork Lesson 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Encoding;
+
+namespace HomeWork_Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Введите число: ");
+            string first = Console.ReadLine();
+            Console.Write("А теперь второе число: ");
+            string second = Console.ReadLine();
+            
+        }
+    }
+}
