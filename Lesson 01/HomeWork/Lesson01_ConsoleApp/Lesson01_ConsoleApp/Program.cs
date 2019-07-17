ddb2021a0ae71136ec2a2f04ae77ef63d2789787
Ivanavatkov <vanes700i@yandex.ru>
2019-07-17 16:28:20 +0300

    HomeWork Lesson01 added

---
+++ Lesson 01/HomeWork/Lesson01_ConsoleApp/Lesson01_ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace Lesson01_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Пожалуйста, подождите...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"До свидания, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
