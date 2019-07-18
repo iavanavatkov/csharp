7dac366001f540fb829909547b5afb6429942f61
ivanavatkov <52930593+iavanavatkov@users.noreply.github.com>
2019-07-19 02:17:25 +0300

    CleanComments

--- Lesson 02/HomeWork/HomeWork Lesson 2/HomeWork Lesson 2/Program.cs
+++ Lesson 02/HomeWork/HomeWork Lesson 2/HomeWork Lesson 2/Program.cs
@@ -20,21 +20,13 @@ namespace Lesson01_ConsoleApp
             firstNumber = firstNumber.Replace(".", ","); //Чтобы при парсинге на точки не ругался.
             secondNumber = secondNumber.Replace(".", ",");
             {
-                //int secondNumberInt = int.Parse(secondNumber); 
-                //bool checkZero = true;
-                //while (checkZero) ухххххх циклы аыаааа, пока забуду про них
                 if (secondNumber == "0")//либо можно поставить ноль в кавычки, чтобы не парсить никуда. И надо попробовать сделать цикл, чтобы назад к вводу возвращало.
                 {
-                    //checkZero = false;
                     Console.WriteLine("Второе число не может быть нулём");
 
                 }
                 else
-                { //Пока что только так смог реализовать.
-                  /* если число меньше диапазона децимала, то можно парсить в меньшее.
-                  И нужно приравнять точку к запятой, а то иначе он не понимает, что при написании точки не целое число. Или заменить точку на запятую как-нибудь.
-                  И если вводится текст, выдавать сообщение об ошибке, а не просто крашить. */
-                    decimal firstNumberParsed = decimal.Parse(firstNumber);
+                { decimal firstNumberParsed = decimal.Parse(firstNumber);
                     decimal secondNumberParsed = decimal.Parse(secondNumber);
                     Console.WriteLine($"Деление: {firstNumber}/{secondNumber} = {firstNumberParsed / secondNumberParsed};");
                     Console.WriteLine($"Умножение: {firstNumber}*{secondNumber} = {firstNumberParsed * secondNumberParsed};");
