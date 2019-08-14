4b380ad88da6210966e655e378269585c736c9dc
Ivan <vanes700i@yandex.ru>
2019-08-14 16:23:44 +0300

    HomeWork04Added

---
+++ Lesson04/ClassWork/ColorsThirdTry/ColorsThirdTry/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    [Flags]
+    enum colors
+    {
+        None = 0x0,
+        Black = 0x1,
+        White = 0x1 << 1,
+        Cyan = 0x1 << 2,
+        Blue = 0x1 << 3,
+        Green = 0x1 << 4,
+        Red = 0x1 << 5,
+        Yellow = 0x1 << 6,
+        Magenta = 0x1 << 7,
+        Gray = 0x1 << 8,
+        Brown = 0x1 << 9,
+        Violet = 0x1 << 10,
+        Crimson = 0x1 << 11
+    }
+
+    static void Main(string[] args)
+    {
+        // рассчитываем переменную со всеми возможными цветами
+        colors allColors = 0;
+        foreach (colors singleColor in Enum.GetValues(typeof(colors)))
+        {
+            allColors = allColors | singleColor;
+        }
+
+        // выводим возможные цвета за исключением "нулевого"
+        Console.WriteLine("Список возможных цветов:");
+        Console.WriteLine(allColors);
+
+        // здесь будем хранить любимые цвета
+        colors favoriteColors = 0;
+
+        // прочитаем их
+        for (int i = 0; i < 4; i++)
+        {
+            Console.Write($"Введите любимый цвет {i + 1}: ");
+
+            object color = Enum.Parse(typeof(colors), Console.ReadLine());
+            favoriteColors |= (colors)color;
+        }
+
+        // выводим любимые цвета
+        Console.WriteLine("Любимые цвета:");
+        Console.WriteLine(favoriteColors);
+        Console.WriteLine();
+
+        // рассчитываем нелюбимые цвета черех allColors XOR favoriteColors.
+        colors notFavoriteColors = allColors ^ favoriteColors;
+
+        // можно также инвертировать favoriteColors,
+        // а потом "просеять" через AND с allColors.
+        //colors notFavoriteColors = ~favoriteColors;
+        //notFavoriteColors = (colors)(((int)notFavoriteColors << 23) >> 23 );
+
+        Console.WriteLine("Нелюбимые цвета:");
+        Console.WriteLine(notFavoriteColors);
+        Console.WriteLine();
+
+        Console.WriteLine("\nОтладочная информация:");
+        WriteInt32ValueWithBits(allColors, nameof(allColors));
+        WriteInt32ValueWithBits(favoriteColors, nameof(favoriteColors));
+        WriteInt32ValueWithBits(notFavoriteColors, nameof(notFavoriteColors));
+    }
+
+    static void WriteInt32ValueWithBits(object value, string description)
+    {
+        Console.WriteLine(
+            "Flags: {0} : {1}",
+            Convert.ToString((int)value, 2).PadLeft(32, '0'),
+            description);
+    }
+}
---
+++ Lesson04/HomeWork/JuiceContainer/JuiceContainer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JuiceContainer
+{
+	class Program
+	{
+        enum Container
+		{
+			None = 0,
+            SmallContainer = 1,
+            MiddleContainer = 5,
+            BigContainer = 20
+		}
+
+
+		static void Main(string[] args)
+
+		{
+			try
+			{
+				Console.Write("Введите количество литров сока: ");
+                string input = Console.ReadLine();
+                double inputParsed = double.Parse(input);
+
+                double bigContainerDouble = Math.Ceiling(Convert.ToDouble(Container.BigContainer)); //парсить через double.Parse(Container.BigContainer) почему-то не получилось.
+				double bigContainerOstatok = inputParsed % (double)Container.BigContainer;
+
+				double middleContainerDouble = Math.Ceiling(Convert.ToDouble(Container.MiddleContainer));
+				double middleContainerOstatok = bigContainerOstatok % (double)Container.MiddleContainer;
+
+				double smallContainerDouble = Math.Ceiling(Convert.ToDouble(Container.SmallContainer)); //остаток от него вычислять смысла не имеет.
+
+
+				if (inputParsed >= bigContainerDouble)
+				{
+					Console.WriteLine($"Вам необходимо {Math.Floor(inputParsed / bigContainerDouble)} двадцатилитровых контейнера");
+				}
+
+				if (bigContainerOstatok >= middleContainerDouble)
+				{
+					Console.WriteLine($"Вам необходимо {Math.Floor(bigContainerOstatok / middleContainerDouble)} пятилитровых контейнера");
+				}
+
+				if (middleContainerOstatok >= smallContainerDouble)
+				{
+					Console.WriteLine($"Вам небходимо {Math.Ceiling(middleContainerOstatok / smallContainerDouble)} литровых контейнера");
+				}
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine($"Произошла ошибка '{e.Message}'");
+			}
+		}
+	}
+}
