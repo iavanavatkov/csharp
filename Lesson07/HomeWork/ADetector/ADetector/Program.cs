41c96630e3838e6853b30847e4c16f2e42fa384c
Ivan <vanes700i@yandex.ru>
2019-09-02 18:49:31 +0300

    Lesson07HomeWorkAdded

---
+++ Lesson07/HomeWork/ADetector/ADetector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADetector
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Lesson07/HomeWork/Inverse/Inverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Inverse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.Write("Введите слово для того, чтобы инвертировать его. " +
+							  "Напишите \"Выход\", чтобы остановить работу программы:  ");
+				string input = Console.ReadLine(); //Могу ли я сделать ToLower прямо здесь?
+				input = input.ToLower();
+				if (input == "выход")
+				{
+					break;
+				}
+				for (int inverse = input.Length; inverse > 0; inverse--)
+				{
+					Console.Write(input[inverse - 1]);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
